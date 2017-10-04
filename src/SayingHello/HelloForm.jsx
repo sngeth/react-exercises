7765ed737c1d378d9d21204a53559e4589b26ac3
Sid Ngeth <sngeth@gmail.com>
2017-10-03 21:53:41 -0400

    Saying Hello

---
+++ src/SayingHello/HelloForm.jsx
@@ -0,0 +1,38 @@
+Rmport React from 'react'
+import Greeter from './Greeter'
+
+class HelloForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {name: '', submitted: false};
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value, submitted: false });
+  }
+
+  handleSubmit(event) {
+    this.setState({ submitted: true });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            What is your name?
+            <input type="text" onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        { this.state.submitted ? <Greeter name={this.state.name} /> : null }
+      </div>
+    );
+  }
+}
+
+export default HelloForm
