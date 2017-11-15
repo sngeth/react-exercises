ce4ac29972cad682e72ddceda48c6f21d1b1ad20
Sid Ngeth <sngeth@gmail.com>
2017-11-14 22:28:54 -0500

    Tweak exercises into 3 col grid

--- src/Shared/Home.jsx
+++ src/Shared/Home.jsx
@@ -6,64 +6,65 @@ const Home = () => {
     <div className="album text-muted">
       <div className="container">
         <div className="row">
-          <div className="card">
+          <div className="col">
             <Link to={{ pathname: '/saying_hello' }}>
               Saying Hello
             </Link>
-            <p className="card-text">Tell me your name and I'll say Hello</p>
+            <p>Tell me your name and I'll say Hello</p>
           </div>
-
-          <div className="card">
+          <div className="col">
             <Link to={{ pathname: '/counting_characters' }}>
               Counting Characters
             </Link>
-            <p className="card-text">Give me some text and i'll count the number of characters</p>
+            <p>Give me some text and i'll count the number of characters</p>
           </div>
-
-          <div className="card">
+          <div className="col">
             <Link to={{ pathname: '/printing_quotes' }}>
               Printing Quotes
             </Link>
-            <p className="card-text">Print a quote</p>
+            <p>Print a quote</p>
           </div>
-
-          <div className="card">
+        </div>
+        <div className="row">
+          <div className="col">
             <Link to={{ pathname: '/mad_lib' }}>
               Mad Lib
             </Link>
-            <p className="card-text">Generate a mad lib</p>
+            <p>Generate a mad lib</p>
           </div>
-
-          <div className="card">
+          <div className="col">
             <Link to={{ pathname: '/simple_math' }}>
              Simple Math
             </Link>
-            <p className="card-text">Do some basic addition, subtraction, multiplication, and division</p>
+            <p>Do some basic addition, subtraction, multiplication, and division</p>
           </div>
-          <div className="card">
-            <Link to={{ pathname: '/retirement_calculator' }}>
-             Retirement Calculator
-            </Link>
-            <p className="card-text">Calculate the years and date until retirement</p>
+          <div className="col">
+              <Link to={{ pathname: '/retirement_calculator' }}>
+               Retirement Calculator
+              </Link>
+              <p>Calculate the years and date until retirement</p>
           </div>
-
-          <div className="card">
-            <Link to={{ pathname: '/room_area' }}>
-             Area of a Rectangular Room
-            </Link>
-            <p className="card-text">Calculate Room Area in Sq. Ft and Sq. M</p>
+        </div>
+        <div className="row">
+          <div className="col">
+              <Link to={{ pathname: '/room_area' }}>
+               Area of a Rectangular Room
+              </Link>
+              <p>Calculate Room Area in Sq. Ft and Sq. M</p>
           </div>
-          <div className="card">
-            <Link to={{ pathname: '/pizza_party' }}>Pizza Party</Link>
-            <p className="card-text">Plan a pizza party</p>
+          <div className="col">
+              <Link to={{ pathname: '/pizza_party' }}>Pizza Party</Link>
+              <p>Plan a pizza party</p>
           </div>
-          <div className="card">
-          <Link to={{ pathname: '/paint_calculator' }}>Paint Calculator</Link>
-            <p className="card-text">Calculate paint needed to paint a room</p>
+          <div className="col">
+            <Link to={{ pathname: '/paint_calculator' }}>Paint Calculator</Link>
+              <p>Calculate paint needed to paint a room</p>
           </div>
-          <div className="card">
-          <Link to={{ pathname: '/self_checkout' }}>Self Checkout</Link>
-            <p className="card-text">Simple check-out system</p>
+        </div>
+        <div className="row">
+          <div className="col">
+            <Link to={{ pathname: '/self_checkout' }}>Self Checkout</Link>
+              <p>Simple check-out system</p>
           </div>
         </div>
       </div>
